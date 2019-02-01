01af352fb33af49649fae2a29f2f4bb6b3508267
Les Aker <me@lesaker.org>
2019-02-01 11:35:35 -0500

    newlines are cool

--- log/writer.go
+++ log/writer.go
@@ -1,12 +1,13 @@
 package log
 
 import (
+	"fmt"
 	"os"
 )
 
 type stderrWriter struct{}
 
 func (s stderrWriter) Write(str *string) error {
-	_, err := os.Stderr.WriteString(*str)
+	_, err := fmt.Fprintln(os.Stderr, str)
 	return err
 }
