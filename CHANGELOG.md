bd1e4c9b96bb1e74644e02051c6fd52f16199436
Les Aker <50016312+akerl-unpriv@users.noreply.github.com>
2019-08-12 08:03:13 +0300

    Fix/stringbuilder (#2)
    * fix string builder method
    
    * add changelog entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 2.0.1 / 2019-08-12
+
+* [BUGFIX] Fix string encoder to correctly reference variable
+
 # 2.0.0 / 2019-08-09
 
 * Initial stable release
--- log/encoder.go
+++ log/encoder.go
@@ -11,14 +11,14 @@ import (
 type stringEncoder struct{}
 
 func (s stringEncoder) Encode(e events.Event) (string, error) {
-	var b *strings.Builder
+	var b strings.Builder
 	var res string
 	_, err := b.WriteString(e.Time.Format(time.RFC3339))
 	if err != nil {
 		return res, err
 	}
 	for k, v := range e.Fields {
-		_, err := fmt.Fprintf(b, " %s=%s", k, v)
+		_, err := fmt.Fprintf(&b, " %s=%s", k, v)
 		if err != nil {
 			return res, err
 		}
